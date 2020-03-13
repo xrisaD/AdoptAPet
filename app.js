e1a298438099e335c189fbff637e29e88e1cfecd
xrisaD <xrdikonimaki@hotmail.com>
2020-03-13 22:32:45 +0200

    we ate our faces with css

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set("view engine" , "ejs");
 var pets = [
 		{name : "Suzie" , image : "https://cdn.royalcanin-weshare-online.io/pCJJPmYBaxEApS7LeAbn/v1/ed7h-how-to-buy-a-puppy-hero-dog"},
 		{name : "Max" , image : "https://static.scientificamerican.com/sciam/cache/file/D059BC4A-CCF3-4495-849ABBAFAED10456_source.jpg?w=590&h=800&526ED1E1-34FF-4472-B348B8B4769AB2A1"},
+		{name : "Rex" , image : "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"},
+		{name : "Suzie" , image : "https://cdn.royalcanin-weshare-online.io/pCJJPmYBaxEApS7LeAbn/v1/ed7h-how-to-buy-a-puppy-hero-dog"},
+		{name : "Max" , image : "https://static.scientificamerican.com/sciam/cache/file/D059BC4A-CCF3-4495-849ABBAFAED10456_source.jpg?w=590&h=800&526ED1E1-34FF-4472-B348B8B4769AB2A1"},
 		{name : "Rex" , image : "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"}
 	]; 
 
